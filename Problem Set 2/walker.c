3d589220d7aa567776f34180e53e13e41411d2ab
vkuruturi <kuruturi.venkat@gmail.com>
2015-09-23 11:02:10 -0400

    added print permissions function

--- Problem Set 2/walker.c
+++ Problem Set 2/walker.c
@@ -15,9 +15,78 @@
 #include <grp.h>    //for group struct
 #include <string.h>	//for stringcmp
 
+int filetype(mode_t st_mode){
+   char    c;
+
+    if (S_ISREG(st_mode))		//
+        c = '-';
+    else if (S_ISDIR(st_mode))	//
+        c = 'd';
+    else if (S_ISBLK(st_mode))	//
+        c = 'b';
+    else if (S_ISCHR(st_mode))
+        c = 'c';
+	#ifdef S_ISFIFO
+    else if (st_S_ISFIFO(st_mode))
+        c = 'p';
+	#endif  /* S_ISFIFO */
+	#ifdef S_ISLNK
+    else if (st_S_ISLNK(st_mode))
+        c = 'l';
+	#endif  /* S_ISLNK */
+	#ifdef S_ISSOCK
+    else if (S_ISSOCK(st_mode))
+        c = 's';
+	#endif  /* S_ISSOCK */
+	#ifdef S_ISDOOR
+    /* Solaris 2.6, etc. */
+    else if (S_ISDOOR(st_mode))
+        c = 'D';
+	#endif  /* S_ISDOOR */
+    else
+    {
+        /* Unknown type -- possibly a regular file? */
+        c = '?';
+    }
+    return(c);
+}
+
+char* readPermissions(mode_t st_mode){
+	int i = 0;
+    static const char *rwx[] = {"---", "--x", "-w-", "-wx",
+    	"r--", "r-x", "rw-", "rwx"};
+
+	static char perm[11];
+	perm[10] = '\0';
+
+    perm[0] = filetype(st_mode);
+    strcpy(&perm[1], rwx[(st_mode >> 6)& 7]);
+    strcpy(&perm[4], rwx[(st_mode >> 3)& 7]);
+    strcpy(&perm[7], rwx[(st_mode & 7)]);
+    if (mode & S_ISUID)
+        perm[3] = (st_mode & S_IXUSR) ? 's' : 'S';
+    if (mode & S_ISGID)
+        perm[6] = (st_mode & S_IXGRP) ? 's' : 'l';
+    if (mode & S_ISVTX)
+        perm[9] = (st_mode & S_IXUSR) ? 't' : 'T';
+
+}
+
+int output(char *path, struct stat *st, char *user, int mtime){
+	int index = 0;
+	struct passwd *pw = getpwuid(st->st_uid);
+	if(!(user) || (strcmp(user, pw->pw_name) ==0) || (atoi(user) == st->st_uid)){
+		if((mtime == 0) || ((mtime < 0) && (difftime(time(NULL), st->st_ctime) < (-mtime))) || ((mtime > 0) && (difftime(time(NULL), st->st_ctime) > mtime))){
+			printf("%04x/", st->st_dev);	//device driver printed as hex with 4 digits min
+			printf("%-7i ", st->st_ino);	//inode number
+			printf("%s ", readPermissions(st->st_mode));
+		}
+	}
+}
+
 int main(int argc, char* argv[]){
 	char *user = NULL;		//username
-	int mtime =0;			//modified filter				
+	int mtime = 0;			//modified filter				
 
 	//parse input arguments for -u, -m and their following arguments
 	while(char ch = getopt(argc, argv, "u:m") != -1){
@@ -52,5 +121,5 @@ int main(int argc, char* argv[]){
 	}
 
 	if((st.st_mode &S_IFMT) == S_IFLNK)
-		printInfo(argv[optind], &st, user, mtime)
+		output(argv[optind], &st, user, mtime)
 }
